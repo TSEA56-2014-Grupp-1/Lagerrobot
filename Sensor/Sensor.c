756ca1ca86ea91acf41cd319c33cfa3e7e76b79e
Karl Linderhed <karli315@student.liu.se>
2014-03-27 08:56:32 +0100

    Initial sensor commit

---
+++ Sensor/Sensor.c
@@ -0,0 +1,17 @@
+/*
+ * Sensor.c
+ *
+ * Created: 2014-03-27 08:55:21
+ *  Author: Karl
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
