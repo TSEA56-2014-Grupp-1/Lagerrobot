57efd41166c323f65a542344891652c33f556d69
Karl Linderhed <karli315@student.liu.se>
2014-03-28 16:56:05 +0100

    Added lcd test project for bus communication

---
+++ lcd_test/lcd_test.c
@@ -0,0 +1,24 @@
+/*
+ * lcd_test.c
+ *
+ * Created: 3/28/2014 4:34:09 PM
+ *  Author: karli315
+ */ 
+
+
+#include <avr/io.h>
+//#include "bus.h"
+
+/*uint16_t symbol_request(uint8_t id, uint16_t data) {
+	
+	return ((uint16_t)'H' << 8) | ((uint16_t)'A');
+}*/
+
+int main(void)
+{
+	//bus_init(0b0000011);
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
