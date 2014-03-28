ead5bf64d57bfd3fed23308e07a23540bba68b77
Karl Linderhed <karli315@student.liu.se>
2014-03-28 16:54:57 +0100

    added message rotation

--- Communication/Communication.c
+++ Communication/Communication.c
@@ -4,28 +4,71 @@
  * Created: 2014-03-26 09:49:31
  *  Author: Karl
  */ 
-
+#define ROTATE_INTERVAL 10
 
 #include <avr/io.h>
 #include "LCD.h"
+#include "bus.h"
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+uint8_t lcd_rotation_counter = 0;
+uint8_t lcd_current_sender = 0;
+
+char message_map_line1[4][13] = {
+	"Kommunikation",
+	"Sensor",
+	"Arm",
+	"Chassi"};
+	
+char message_map_line2[4][16] = {
+	"Kommunicerar",
+	"Censurerar",
+	"Armerar",
+	"Charkuterierar"};
+
+ISR(TIMER1_OVF_vect) {
+	if(lcd_rotation_counter == ROTATE_INTERVAL) {
+		lcd_rotation_counter = 0;
+		
+		display(lcd_current_sender,
+				message_map_line1[lcd_current_sender],
+				message_map_line2[lcd_current_sender]);
+		if (lcd_current_sender == 3)
+			lcd_current_sender = 0;
+		else
+			++lcd_current_sender;
+	}
+	else
+		++lcd_rotation_counter;
+}
 
 void init(){
 	DDRA = 0xff;
 	DDRB = 0xff;
 	PORTB = 0b00000000;
 	PORTA = 0x0;
+	
+	TIMSK1 = (1 << TOIE1);
+	TCCR1A = 0x00; // normal mode
+	TCCR1B = 0b00000010; // normal mode, max prescaler; 
+	
 }
+
 
 int main(void)
 {
 	init();
-	//_delay_ms(200);
 	lcd_init();
+	bus_init(0b0000101);
 	
-	lcd_print("Are you done?", "I want to live!");
+	
+	
+	sei();
+	display(COMM, "Hello!", "World!");
     while(1)
     {
+		
         //TODO:: Please write your application code 
     }
 }
--- Communication/LCD.h
+++ Communication/LCD.h
@@ -9,6 +9,14 @@
 #ifndef LCD_H_
 #define LCD_H_
 
+#define F_CPU 18.432E6
+#define COMM 0
+#define SENS 1
+#define ARM 2
+#define CHAS 3
+
+#include <avr/io.h>
+
 void lcd_send_symbol(char);
 void lcd_send_command(char);
 int lcd_is_busy();
@@ -18,6 +26,6 @@ void lcd_clear();
 
 void lcd_init();
 
-
+void display(uint8_t sender, char text1[], char text2[]);
 
 #endif /* LCD_H_ */
