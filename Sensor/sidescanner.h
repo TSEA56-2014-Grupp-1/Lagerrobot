d2448fcd2c8238182bd31fc12ec1ccb860d4c926
Philip <ph.nilsson@gmail.com>
2014-04-03 11:29:45 +0200

    Forgot to add sidescanner.h

---
+++ Sensor/sidescanner.h
@@ -0,0 +1,21 @@
+﻿/*
+ * sidescanner.h
+ *
+ * Created: 4/3/2014 10:04:08 AM
+ *  Author: phini326
+ */ 
+
+
+#ifndef SIDESCANNER_H_
+#define SIDESCANNER_H_
+
+void sidescanner_init();
+uint8_t scanner_left_position(uint8_t);
+uint8_t scanner_right_position(uint8_t);
+uint8_t sweep_left();
+uint8_t sweep_right();
+
+void update_distance();
+
+
+#endif /* SIDESCANNER_H_ */
