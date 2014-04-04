8f51939a37eeb9ab2b2388227a85dddabeca2732
Johan <johli887@student.liu.se>
2014-04-04 15:50:16 +0200

    first initial commit

---
+++ inverse_kinematics/inverse_kinematics/inverse_kinematics.h
@@ -0,0 +1,24 @@
+﻿/*
+ * inverse_kinematics.h
+ *
+ * Created: 4/4/2014 2:01:54 PM
+ *  Author: johli887
+ */ 
+
+
+#ifndef INVERSE_KINEMATICS_H_
+#define INVERSE_KINEMATICS_H_
+
+#define LENGTH_OF_ARM_1 18
+#define LENGTH_OF_ARM_2 17
+#define LENGTH_OF_ARM_3	8
+
+typedef struct {
+	uint8_t x;
+	uint8_t y;
+}  coordinate;
+
+coordinate calculate_coordinate(int theta1, int theta2, int theta3);
+
+
+#endif /* INVERSE_KINEMATICS_H_ */
