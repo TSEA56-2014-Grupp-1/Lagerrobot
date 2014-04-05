5f439e35054595cde9387c4f9faf3c41ec369a7d
Karl Linderhed <karli315@student.liu.se>
2014-04-05 15:22:23 +0200

    Removed macro in lcd display code, wasn't necessary

--- shared/LCD_interface.h
+++ shared/LCD_interface.h
@@ -3,13 +3,12 @@
 
 #include <avr/io.h>
 #include <stdarg.h>
-#define display(line_number, string, ...) _display(line_number, string, sizeof((float[]){0, ##__VA_ARGS__}) / sizeof(float) - 1, ##__VA_ARGS__)
 
 char lcd_display_symbols[2][17];
 
 void lcd_interface_init();
 
-void _display(uint8_t line_number, const char* str, uint8_t num_vals, ...);
+void display(uint8_t line_number, const char* str, ...);
 
 uint16_t symbol_request(uint8_t id, uint16_t data);
 
