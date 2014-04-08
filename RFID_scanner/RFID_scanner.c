ab64d1ad94806d05a02377d06099666e5d1514e3
eriknybom <erik.nybom@gmail.com>
2014-04-08 14:16:50 +0200

    RFID reading working, compare-function is still unclear

--- RFID_scanner/RFID_scanner.c
+++ RFID_scanner/RFID_scanner.c
@@ -8,6 +8,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define F_CPU 16000000UL
+#include <util/delay.h>
 #include "RFID_scanner.h"
 #include "../shared/usart.h"
 #include "../shared/bus.h"
@@ -19,7 +21,7 @@ uint8_t carrying_RFID[12];
 
 void RFID_scanner_init()
 {
-	DDRD = 0b0000100; // Set PD2 as Output
+	DDRD = 0b11100100; // Set PD2 as Output and pd6 as output for testing XXX
 }
 
 void station_to_LCD(uint8_t station){
@@ -67,19 +69,21 @@ void read_RFID()
 	uint8_t i = 0;
 	for (i = 0; i <= 11; ++i) // Read 12 bytes
 	{
-		//usart_read_byte(&station_RFID[i]);
+		usart_read_byte(&station_RFID[i]);
 	}
-	PORTD = PORTD & (1 << PORTD2); // Disable reading
+	PORTD |= (1 << PORTD2); // Disable reading
 }
 
 
 uint8_t compare_RFID_arrays(uint8_t station_RFID[12], const uint8_t current_compare_RFID[12])
 {
 	uint8_t i;
-	for (i = 0; i <=11; ++i)
+	for (i = 1; i <= 11; ++i)
 	{
-		if(current_compare_RFID[i] != current_compare_RFID[i])
-		return 0;
+		if(station_RFID[i] != current_compare_RFID[i])
+		{
+			return 0;
+		}
 	}
 	return 1;
 }
@@ -99,20 +103,25 @@ uint8_t identify_station_RFID()
 	else if (compare_RFID_arrays(station_RFID, RFID_B85))
 	return 85;
 	else
-	return 0;
+	return 1;
 };
 
-
+uint8_t station = 0;
 int main(void)
 {
 	RFID_scanner_init();
+	PORTD = 0;
+	DDRA = 0b11111111;
+	PORTA = 0;
 	usart_init(520);
-	bus_init(4); //Bus address sensor
-	lcd_interface_init();
 	sei();
+	_delay_ms(200);
+	_delay_ms(200);
 	read_RFID();
-	uint8_t station = identify_station_RFID();
-	station_to_LCD(station);
+	//station = identify_station_RFID();
+	//station_to_LCD(station);
+	
+PORTA = station_RFID[8];
     while(1)
     {
         //TODO:: Please write your application code 
