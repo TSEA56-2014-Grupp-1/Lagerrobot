8da83359e88ae59092814d60874c444260cd8493
Johan <johli887@student.liu.se>
2014-04-10 17:10:11 +0200

    the python code has to be converted to c, still missing about half

--- arm/inverse_kinematics.c
+++ arm/inverse_kinematics.c
@@ -21,19 +21,47 @@ uint16_t ik_rad_to_servo_angle(float rad, uint16_t ref_position) {
 	return 0;
 }
 
-uint8_t ik_coordinate_to_angles(coordinate coord, uint8_t solution, angles *joint_angles) {
+coordinate find_p(coordinate coord)
+{
+	coordinate temp;
+	if ((coord.x - ARM_LENGTH_LINK_3) < X_LIMIT || coord.y <= Y_LIMIT)
+	{
+		temp.x = X_LIMIT;
+		temp.y = coord.y + sqrt(ARM_LENGTH_LINK_3^2 - (coord.x - X_LIMIT)^2);
+		return temp;
+	}
+	else 
+	temp.x = coord.x - ARM_LENGTH_LINK_3;
+	temp.y = coord.y;
+	return temp;
+}
+
+float dot_product(coordinate a, coordinate b)
+{
+	return a.x * b.x + a.y * b.y;
+}
+
+coordinate vector_between(coordinate a, coordinate b)
+{
+	return (coordinate){.x = a.x - b.x, .y = a.y - b.y};
+}
+
+float vector_length(coordinate a)
+{
+	return sqrt(dot_product(a,a));
+}
+
+
+uint8_t ik_angles_p(coordinate coord, angles *joint_angles) {
 	float cos_theta2;
 	float sin_theta2;
 
-	float theta2_1;
-	float theta2_2;
+	float theta2;
 
 	float det_cmatrix;
 
-	float cos_theta1_1;
-	float cos_theta1_2;
-	float sin_theta1_1;
-	float sin_theta1_2;
+	float cos_theta1;
+	float sin_theta1;
 
 
 	cos_theta2 = pow(coord.x, 2) + pow(coord.y, 2);
@@ -44,41 +72,40 @@ uint8_t ik_coordinate_to_angles(coordinate coord, uint8_t solution, angles *join
 		return 1;
 	}
 
-	sin_theta2 = sqrt(1 - pow(cos_theta2, 2));
+	sin_theta2 = -sqrt(1 - pow(cos_theta2, 2));
 
 	det_cmatrix = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 + cos_theta2, 2);
 	det_cmatrix += pow(ARM_LENGTH_LINK_2 * sin_theta2, 2);
 
-	cos_theta1_1 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.x;
-	cos_theta1_1 += ARM_LENGTH_LINK_2 * -sin_theta2 * coord.y;
-	cos_theta1_1 /= det_cmatrix;
+	cos_theta1 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.x;
+	cos_theta1 += ARM_LENGTH_LINK_2 * sin_theta2 * coord.y;
+	cos_theta1 /= det_cmatrix;
 
-	cos_theta1_2 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.x;
-	cos_theta1_2 += ARM_LENGTH_LINK_2 * sin_theta2 * coord.y;
-	cos_theta1_2 /= det_cmatrix;
+	sin_theta1 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.y;
+	sin_theta1 -= ARM_LENGTH_LINK_2 * sin_theta2 * coord.x;
+	sin_theta1 /= det_cmatrix;
 
-	sin_theta1_1 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.y;
-	sin_theta1_1 -= ARM_LENGTH_LINK_2 * -sin_theta2 * coord.x;
-	sin_theta1_1 /= det_cmatrix;
-
-	sin_theta1_2 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.y;
-	sin_theta1_2 -= ARM_LENGTH_LINK_2 * sin_theta2 * coord.x;
-	sin_theta1_2 /= det_cmatrix;
-
-	if (solution) {
-		joint_angles->t1 = atan2f(sin_theta1_2, cos_theta1_2);
-		joint_angles->t2 = atan2f(sin_theta2, cos_theta2);
-		joint_angles->t3 = 0.0;
-	} else {
-		joint_angles->t1 = atan2f(sin_theta1_1, cos_theta1_1);
-		joint_angles->t2 = atan2f(-sin_theta2, cos_theta2);
-		joint_angles->t3 = 0.0;
-	}
-
+	joint_angles->t1 = atan2f(sin_theta1, cos_theta1);
+	joint_angles->t2 = atan2f(sin_theta2, cos_theta2);
+		
 	return 0;
 }
 
-//ik_validate_solution()
+uint8_t ik_angles(coordinate coord, angles *joint_angles)
+{
+	coordinate p = find_p(coord);
+	
+	ik_angles_p(p, joint_angles);
+	coordinate j
+	j.x = ARM_LENGTH_LINK_1*cos(joint_angles->t1);
+	j.y = ARM_LENGTH_LINK_1*sin(joint_angles->t1);
+	
+	coordinate u = vector_between(p);
+	coordinate v = vector_between(coord);
+	
+	theta3 = acos(dot_product(u,v)/(vector_length(u)*vector_length(v)));
+	//TODO
+}
 
 float rad_to_deg(float rad) {
 	return rad * 180.0 / M_PI;
