92950713438cb792717ae80b9ee28697ca2e6705
Johan <johli887@student.liu.se>
2014-04-11 12:53:33 +0200

    first proper implementation of IK

--- arm/inverse_kinematics.c
+++ arm/inverse_kinematics.c
@@ -5,12 +5,20 @@
 
 #include "inverse_kinematics.h"
 
-coordinate ik_calculate_coordinate(int theta1, int theta2, int theta3)
+coordinate ik_calculate_coordinate(float theta1, float theta2, float theta3)
 {
 	coordinate temp;
-	temp.x = ARM_LENGTH_LINK_1 * cos(theta1) + ARM_LENGTH_LINK_2*cos(theta2) + ARM_LENGTH_LINK_3*cos(theta3);
-	temp.y = ARM_LENGTH_LINK_1 * sin(theta1) + ARM_LENGTH_LINK_2*sin(theta2) + ARM_LENGTH_LINK_3*sin(theta3);
+	temp.x = ARM_LENGTH_LINK_1 * cos(theta1) + ARM_LENGTH_LINK_2 * cos(theta2) + ARM_LENGTH_LINK_3 * cos(theta3);
+	temp.y = ARM_LENGTH_LINK_1 * sin(theta1) + ARM_LENGTH_LINK_2 * sin(theta2) + ARM_LENGTH_LINK_3 * sin(theta3);
 	return temp;
+}
+
+/**
+ *	Calculate robot's shell edge based on arm rotation. pi/2 means pointing to the left and -pi/2 means pointing to the right
+ */
+float ik_calculate_x_limit(float beta)
+{
+	return SHORTEST_X_DISTANCE_FROM_ORIGO / sin(beta);
 }
 
 /**
@@ -21,19 +29,18 @@ uint16_t ik_rad_to_servo_angle(float rad, uint16_t ref_position) {
 	return 0;
 }
 
-coordinate find_p(coordinate coord)
+coordinate ik_find_p(coordinate coord, float x_limit)
 {
-	coordinate temp;
-	if ((coord.x - ARM_LENGTH_LINK_3) < X_LIMIT || coord.y <= Y_LIMIT)
+	// Limit wrist start coordinate when close to robot body
+	if ((coord.x - ARM_LENGTH_LINK_3) < x_limit || coord.y <= ARM_Y_LIMIT)
 	{
-		temp.x = X_LIMIT;
-		temp.y = coord.y + sqrt(ARM_LENGTH_LINK_3^2 - (coord.x - X_LIMIT)^2);
-		return temp;
+		return (coordinate){
+			.x = x_limit,
+			.y = coord.y + sqrt(pow(ARM_LENGTH_LINK_3, 2) - pow((coord.x - x_limit), 2))
+		};
+	} else {
+		return (coordinate){.x = coord.x - ARM_LENGTH_LINK_3, .y = coord.y};
 	}
-	else 
-	temp.x = coord.x - ARM_LENGTH_LINK_3;
-	temp.y = coord.y;
-	return temp;
 }
 
 float dot_product(coordinate a, coordinate b)
@@ -48,7 +55,7 @@ coordinate vector_between(coordinate a, coordinate b)
 
 float vector_length(coordinate a)
 {
-	return sqrt(dot_product(a,a));
+	return sqrt(dot_product(a, a));
 }
 
 
@@ -56,13 +63,10 @@ uint8_t ik_angles_p(coordinate coord, angles *joint_angles) {
 	float cos_theta2;
 	float sin_theta2;
 
-	float theta2;
-
 	float det_cmatrix;
 
 	float cos_theta1;
 	float sin_theta1;
-
 
 	cos_theta2 = pow(coord.x, 2) + pow(coord.y, 2);
 	cos_theta2 -= pow(ARM_LENGTH_LINK_1, 2) + pow(ARM_LENGTH_LINK_2, 2);
@@ -74,7 +78,7 @@ uint8_t ik_angles_p(coordinate coord, angles *joint_angles) {
 
 	sin_theta2 = -sqrt(1 - pow(cos_theta2, 2));
 
-	det_cmatrix = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 + cos_theta2, 2);
+	det_cmatrix = pow(ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 + cos_theta2, 2);
 	det_cmatrix += pow(ARM_LENGTH_LINK_2 * sin_theta2, 2);
 
 	cos_theta1 = (ARM_LENGTH_LINK_1 + ARM_LENGTH_LINK_2 * cos_theta2) * coord.x;
@@ -91,27 +95,41 @@ uint8_t ik_angles_p(coordinate coord, angles *joint_angles) {
 	return 0;
 }
 
-uint8_t ik_angles(coordinate coord, angles *joint_angles)
+uint8_t ik_angles(coordinate coord,float x_limit, angles *joint_angles)
 {
-	coordinate p = find_p(coord);
+	coordinate p = find_p(coord, x_limit);
+	if (ik_angles_p(p, joint_angles) != 0) {
+		return 1;
+	}
+
+	coordinate j = {
+		.x = ARM_LENGTH_LINK_1 * cos(joint_angles->t1),
+		.y = ARM_LENGTH_LINK_1 * sin(joint_angles->t1)
+	};
+
+	coordinate u = vector_between(j, p);
+	coordinate v = vector_between(p, coord);
 	
-	ik_angles_p(p, joint_angles);
-	coordinate j
-	j.x = ARM_LENGTH_LINK_1*cos(joint_angles->t1);
-	j.y = ARM_LENGTH_LINK_1*sin(joint_angles->t1);
+	float theta3 = acos(dot_product(u, v) / (vector_length(u) * vector_length(v)));
+	coordinate option1 = ik_calculate_coordinate(joint_angles->t1, joint_angles->t2, theta3);
+	coordinate option2 = ik_calculate_coordinate(joint_angles->t1, joint_angles->t2, -theta3);
 	
-	coordinate u = vector_between(p);
-	coordinate v = vector_between(coord);
+	float option1_length = vector_length(vector_between(coord, option1));
+	float option2_length = vector_length(vector_between(coord, option2));
 	
-	theta3 = acos(dot_product(u,v)/(vector_length(u)*vector_length(v)));
-	//TODO
+	if (option1_length > option2_length) {
+		theta3 *= -1;
+	}
+
+	joint_angles->t3 = theta3;
+	return 0;
 }
 
 float rad_to_deg(float rad) {
 	return rad * 180.0 / M_PI;
 }
 
-int main(void)
+/*int main(void)
 {
 	angles ang;
 	coordinate coord = {.x = 100, .y = 100};
@@ -131,3 +149,4 @@ int main(void)
 	printf("Rad t1: %.2f\n", rad_to_deg(ang.t1));
 	printf("Rad t2: %.2f\n", rad_to_deg(ang.t2));
 }
+*/
--- arm/inverse_kinematics.h
+++ arm/inverse_kinematics.h
@@ -11,7 +11,9 @@
 
 #define ARM_LENGTH_LINK_1 100 // 18
 #define ARM_LENGTH_LINK_2 100 // 17
-#define ARM_LENGTH_LINK_3 8
+#define ARM_LENGTH_LINK_3 50 // 8
+#define ARM_Y_LIMIT -10
+#define SHORTEST_X_DISTANCE_FROM_ORIGO 12	 //måste mätas mer noga
 
 typedef struct {
 	float x;
@@ -24,7 +26,17 @@ typedef struct {
 	float t3;
 } angles;
 
-coordinate calculate_coordinate(int theta1, int theta2, int theta3);
+coordinate calculate_coordinate(float theta1, float theta2, float theta3);
+coordinate find_p(coordinate coord, float x_limit);
+float ik_calculate_x_limit(float beta);
+
+float dot_product(coordinate a, coordinate b);
+coordinate vector_between(coordinate a, coordinate b);
+float vector_length(coordinate a);
+
+uint8_t ik_angles_p(coordinate coord, angles *joint_angles);
+uint8_t ik_angles(coordinate coord,float x_limit, angles *joint_angles);
+float rad_to_deg(float rad);
 
 
 #endif /* INVERSE_KINEMATICS_H_ */
