1ce9a87098edd6deeac4cf774e4c42b28cbd349d
Philip <ph.nilsson@gmail.com>
2014-05-13 18:17:02 +0200

    started main program on sensor unit

--- Sensor/Sensor.c
+++ Sensor/Sensor.c
@@ -11,20 +11,12 @@
 #include "linesensor.h"
 #include "../shared/bus.h"
 
+uint8_t sensor_task = 0;
+
 ISR(ADC_vect) {
-	switch (ADMUX & 0b00011111) {
-		case 0 :
+	if ((ADMUX & 0b00011111 == 0)) {
  			update_linesensor_values();
 			pickup_station_detection();
-		case 1 :
-		
-		break;
-		case 2 : 
-		
-		break;
-		case 3 :
-		
-		break;
 	}
 }
 
@@ -35,13 +27,43 @@ int main(void)
 	bus_register_response(3, return_linesensor);
 	bus_register_response(5, set_tape_reference);
 	bus_register_receive(2, calibrate_linesensor);
-	bus_register_receive(9, clear_pickupstation);
-
-	line_init();
+	bus_register_receive(9, set_task_follow_line);
+	bus_register_receive(12, set_task_pickup_left);
+	bus_register_receive(13, set_task_pickup_right);
+	
+	RFID_scanner_init();
+	usart_init(520);
 	sei();
-
     while(1)
     {
-		calculate_line_weight();
+		sensor_task_manager();
     }
+}
+
+void sensor_task_manager()	{
+	
+	switch (sensor_task)	{
+		case 0 :
+			calculate_line_weight();
+		case 1:
+			//pickup station left
+		case 2:
+			//pickup station right
+		}	
+}
+
+void set_task_pickup_left()	{
+	sensor_task = 1;
+	sidescanner_init();
+}
+
+void set_task_pickup_right()	{
+	sensor_task = 2;
+	sidescanner_init();
+}
+
+void set_task_follow_line()	{
+	sensor_task = 0;
+	clear_pickupstation();
+	line_init();
 }
