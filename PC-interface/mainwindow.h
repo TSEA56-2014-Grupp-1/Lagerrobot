48a865f7e3b57c8795ccd633015a7feb77055c8e
patny205 <patny205@student.liu.se>
2014-05-19 11:49:28 +0200

    Removed unnecessary functions.

--- PC-interface/mainwindow.h
+++ PC-interface/mainwindow.h
@@ -44,29 +44,18 @@ public:
 
 	void handle_decision(quint8 decision);
 
-	//XXX: should be private
-
     void add_range_data(quint8 sensor, quint16 new_data);
+
 private slots:
     void keyPressEvent(QKeyEvent *key_pressed);
-
-    void keyReleaseEvent(QKeyEvent *key_relased);
 
     void on_pushButton_forward_pressed();
 
     void on_pushButton_back_pressed();
 
-    void on_pushButton_forward_released();
-
-    void on_pushButton_back_released();
-
     void on_pushButton_left_pressed();
 
-    void on_pushButton_left_released();
-
     void on_pushButton_right_pressed();
-
-    void on_pushButton_right_released();
 
     void on_pushButton_start_line_clicked();
 
